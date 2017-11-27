8ade1a707a7bc878839e803636cd8433c0c9047e
fbalbino <fbalbino@outlook.com>
2017-11-27 19:48:30 -0200

    London App Brewery - Android Dev Course @ Udemy Challenge #2 - Magic 8 Ball

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,48 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button askButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageBall);
+        askButton = findViewById(R.id.askButton);
+
+        final int[] imageArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        Random randomNumberGenerator = new Random();
+        final int number = randomNumberGenerator.nextInt(4);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomNumberGenerator = new Random();
+                final int number = randomNumberGenerator.nextInt(5);
+
+                imageView.setImageResource(imageArray[number]);
+                Log.d("8Ball", "Random number is: " + number);
+            }
+        });
+
     }
 }
